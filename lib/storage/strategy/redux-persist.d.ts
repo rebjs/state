1452dfe6b1f3f39aaaedf6cbd563c9c752bf4789
Wayne Bloss <waynebloss@gmail.com>
2018-12-15 15:21:23 -0500

    v0.13.2

--- lib/storage/strategy/redux-persist.d.ts
+++ lib/storage/strategy/redux-persist.d.ts
@@ -1,8 +1,19 @@
 import { PersistConfig } from "redux-persist";
-import { StorageStrategy } from "../..";
+import { StateStore, StorageStrategy } from "../..";
 export declare type PersistConfigEx = PersistConfig & {
-    preload: (options: PersistConfigEx, slice: string) => any;
+    preload?: (options: PersistConfigEx, slice: string) => any;
+    sync?: boolean | PersistSyncConfig | PersistSyncStarter;
 };
+export interface PersistSyncConfig {
+    /** Keys to exclude from synchronization. */
+    blacklist?: string[];
+    /** The storage area to handle synchronization for.
+     * Default **localStorage**. */
+    storageArea?: Storage;
+    /** Keys to include in sychronization. */
+    whitelist?: string[];
+}
+export declare type PersistSyncStarter = (store: StateStore, persistConfig: PersistConfigEx) => void;
 export declare function createReduxPersistStorage(options: PersistConfigEx): StorageStrategy;
 /** Loads persisted reducer state data from localStorage, synchronously.
  * @param options The redux-persist config options.
