d31284fd13aebf343942f53ea37786026be1fb28
Wayne Bloss <waynebloss@gmail.com>
2018-12-15 15:24:49 -0500

    Update example

--- example/basic/src/state/store.js
+++ example/basic/src/state/store.js
@@ -1,6 +1,7 @@
 import { StateStore } from '@reb/state';
 import {
   createReduxPersistStorage,
+  preloadReduxPersistLocalStorage,
 } from '@reb/state/lib/storage/strategy/redux-persist';
 import { createLogger } from 'redux-logger';
 import persistLocalStorage from "redux-persist/lib/storage";
@@ -19,7 +20,9 @@ export const store = new StateStore({
   storage: createReduxPersistStorage({
     debug: true,
     key: 'reduxPersist',
+    preload: preloadReduxPersistLocalStorage,
     storage: persistLocalStorage,
+    sync: true,
   }),
 });
 export default store;
